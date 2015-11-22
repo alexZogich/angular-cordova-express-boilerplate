93752827b68233df59211892d4584db5289279d1
Oleksandr Tsaruk <Oleksandr_Tsaruk@epam.com>
2015-11-22 21:39:42 +0200

    init: added base project boilerplate

---
+++ client/directives/nav-bar/nav-bar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('resolved')
+  .directive('navBar', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'directives/nav-bar/nav-bar.html'
+    };
+  });
---
+++ client/views/home/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('resolved')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'vm'
+      });
+  });
